2c3521ce3f05395aebd41e761e8e325369f3293f
irsan mansyur <irsan00mansyur@gmail.com>
2023-05-23 05:44:40 +0700

    Test saja

--- src/app/api/telegram/notif/route.ts
+++ src/app/api/telegram/notif/route.ts
@@ -5,5 +5,6 @@ export async function POST(request: NextRequest) {
   const { sendMessage, botChatId } = telegramService();
   // Menangani pesan yang diterima oleh bot
   sendMessage(botChatId, `Anda mengirim pesan: \n${JSON.stringify(payload)}`);
+
   return NextResponse.json({ data: payload ?? {} });
 }
