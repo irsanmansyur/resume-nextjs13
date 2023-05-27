eaefc40be109f4807001af96710cbab3d5b972a0
irsan mansyur <irsan00mansyur@gmail.com>
2023-05-28 00:25:00 +0700

    update favicon and  data profile

--- src/components/social-media.tsx
+++ src/components/social-media.tsx
@@ -1,5 +1,3 @@
-'use client';
-import { Icon } from '@iconify/react';
 import Link from 'next/link';
 import IconSosmed from './sosmed/icon';
 
@@ -7,10 +5,11 @@ type Props = { sosmed: Record<string, any>[] };
 export const SocialMedia = ({ sosmed }: Props) => {
   return (
     <div className="social-media flex items-center gap-6">
-      {sosmed.map((sm) => {
+      {sosmed.map((sm: Record<string, any>, i) => {
         return (
           <Link
-            href={`${sm.url}`}
+            key={i}
+            href={sm.url}
             target="_blank"
             className="tracking-widest bg-transparent relative border-2 border-primary p-2  font-bold z-[1] overflow-hidden before:transition before:duration-500 before:w-full before:-translate-x-full before:hover:translate-x-0 before:z-[-1] before:absolute  before:top-0 before:left-0 before:h-full before:bg-primary rounded-full group"
           >
