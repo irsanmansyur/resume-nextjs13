968f263edae4eac6b78fc708570d8efc1c550e0d
chank <ch4nk.dev@gmail.com>
2023-07-18 15:09:30 +0700

    add await pada saat ngirim api

--- src/app/api/question/route.ts
+++ src/app/api/question/route.ts
@@ -6,7 +6,7 @@ export async function GET() {}
 export async function POST(request: NextRequest) {
   const { data, error } = questionValidate(await request.json());
   if (error) return NextResponse.json({ message: 'Gagal', errors: error }, { status: 422 });
-  axiosInstance.post(`question`, data).catch((err) => {
+  await axiosInstance.post(`question`, data).catch((err) => {
     console.log(err?.message || 'Error Post question');
   });
   return NextResponse.json({ data: 'quastion' });
