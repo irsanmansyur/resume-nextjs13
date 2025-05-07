d0216c7f72cca21cb43af67d2b5c617ca8944293
Irsan Mansyur <irsan00mansyur@gmail.com>
2025-05-07 17:10:07 +0700

    Mock user data in userProvider for development

--- src/utils/providers/user.provider.ts
+++ src/utils/providers/user.provider.ts
@@ -1,10 +1,18 @@
-import axiosInstance from '@/axios-instance';
-import { TApiResponse, TUser } from '@/interfaces';
+import { EGender, TUser } from '@/interfaces';
 
 export function userProvider() {
   const getUser = async () => {
-    const user = await axiosInstance.get<TApiResponse<TUser>>(process.env.API_URL + '/user/chank');
-    return user.data.data;
+    // const user = await axiosInstance.get<TApiResponse<TUser>>(process.env.API_URL + '/user/chank');
+    const user: TUser = {
+      name: 'Irsan Mansyur',
+      username: 'chank',
+      email: 'irsan00mansyur@gmail.com',
+      password: '',
+      gender: EGender.MALE,
+      about: 'saya seorang pengembang web. mahir menggunakan php dan nodejs, saya bisa menggunakan framework codeigniter dan laravel, untuk nodejs saya menggunakan nestjs, reactjs, jquery dan nextjs, dll',
+      sosmed: [{ name: 'Facebook', url: 'https://facebook.com/ichank00' }],
+    };
+    return user;
   };
   return { getUser };
 }
